7fb720e3f1763f97751eedeccdb0aa43cd79d0d4
Projectroar <projectroooaaarrr@gmail.com>
2024-07-05 01:58:11 +0900

    c 연습

--- Project1/Project1/test.c
+++ Project1/Project1/test.c
@@ -3,16 +3,6 @@
 int main(void)
 {
 	
-	printf("this is line1...\n");
-	printf("this is line2...\n");
-	printf("this is line3...\n");
-	printf("this is line4...\n");
-	printf("this is line5...\n");
-	printf("this is line6...\n");
-	printf("this is line7...\n");
-	printf("this is line8...\n");
-	printf("this is line9...\n");
-	printf("this is line10...\n");
-	
+	//우효오오오옷!!!
 	return 0;
 }
