ff9f25da5dfc0a7b1a6624ad4bb3eaed434f9ac0
Projectroar <projectroooaaarrr@gmail.com>
2024-07-05 01:09:22 +0900

    c 연습파일

---
+++ Project1/Project1/test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	
+	printf("this is line1...\n");
+	printf("this is line2...\n");
+	printf("this is line3...\n");
+	printf("this is line4...\n");
+	printf("this is line5...\n");
+	printf("this is line6...\n");
+	printf("this is line7...\n");
+	printf("this is line8...\n");
+	printf("this is line9...\n");
+	printf("this is line10...\n");
+	
+	return 0;
+}
