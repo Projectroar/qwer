ba0e61a14e39405cf2ea71c141d804e0d88f5b11
Projectroar <projectroooaaarrr@gmail.com>
2024-07-17 21:02:57 +0900

    뭔지 모르겠네

---
+++ Project3/Project3/tast.c
@@ -0,0 +1,16 @@
+#include<stdio.h>//표준입출력
+#include<stdlib.h>//system함수용, rand포함
+#include<limits.h>//정수형리미트
+#include<float.h>//실수형리미트
+#include<string.h>//문자열카피(strcpt_s)
+
+
+
+int main(void){
+	system("gnuplot.exe");
+	system("pause");
+	return 0;
+}
+
+
+
